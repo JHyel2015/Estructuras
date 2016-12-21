d5b5d3cc0d4108d347baacdc9a4d8a08d8b54ccb
JHyel2015 <elgeorh@gmail.com>
2016-12-21 11:01:16 -0500

    editor

--- src/CorreccionPrueba/Materia.java
+++ src/CorreccionPrueba/Materia.java
@@ -1,9 +1,18 @@
 package CorreccionPrueba;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Materia {
-	
+
+	public Profesor[] getProfesores() {
+		return profesores;
+	}
+
+	public void setProfesores(Profesor[] profesores) {
+		this.profesores = profesores;
+	}
+
 	private String codigo;
 	private String nombre;
 	private int numHoras;
@@ -18,8 +27,59 @@ public class Materia {
 		this.nombre="materia1";
 		this.numHoras=2;
 		this.profesores=new Profesor[] {new Profesor(),new Profesor()};
-		this.alumnos=new Alumno[] {new Alumno(),new Alumno()};
-		
+		this.alumnos=new Alumno[] {new Alumno(),new Alumno()};		
+	}
+	
+
+	public String getCodigo() {
+		return codigo;
 	}
 
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumHoras() {
+		return numHoras;
+	}
+
+	public void setNumHoras(int numHoras) {
+		this.numHoras = numHoras;
+	}
+
+	public Alumno[] getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(Alumno[] alumnos) {
+		this.alumnos = alumnos;
+	}
+	
+	@Override
+	public String toString() {
+		return "Materia [codigo=" + codigo + ", nombre=" + nombre
+				+ ", numHoras=" + numHoras + ", alumnos="
+				+ Arrays.toString(alumnos) + ", profesores="
+				+ Arrays.toString(profesores) + "]";
+	}
+	
+	public Profesor[]buscarProfesores(String idProfesor){
+		for(Profesor p:profesores){
+			if(p.getId().equals(idProfesor)){
+				return
+			}
+		}
+	}
+	
+
+	
+	
 }
--- src/CorreccionPrueba/Sistema.java
+++ src/CorreccionPrueba/Sistema.java
@@ -2,4 +2,12 @@ package CorreccionPrueba;
 
 public class Sistema {
 	private Materia [] materias;
+	
+	public Materia[] getMaterias(){
+		return materias;
+	}
+	public void setMaterias(Materia[]materias){
+		this.materias=materias;
+	}
+	public M
 }
